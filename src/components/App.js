49dda62e20e31b7f0188dc5c72df62031da16e3d
Govind <agovindchavanb@gmail.com>
2023-06-23 08:42:58 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import './../styles/App.css';
 import LapTimer from "./Laptimer";
@@ -5,6 +6,7 @@ import LapTimer from "./Laptimer";
 const App = () => {
   return (
     <div>
+        {/* Do not remove the main div */}
         <LapTimer/>
     </div>
   )
